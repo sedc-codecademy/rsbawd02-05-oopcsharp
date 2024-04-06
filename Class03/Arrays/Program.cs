e4e57a58e95433a46d9ff70a8199617b25f9dd8d
Almir Vuk <almir.vuk@outlook.com>
2024-04-06 12:05:06 +0200

    Class03 code added.

---
+++ Class03/Arrays/Program.cs
@@ -0,0 +1,121 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fixed size array
+            int[] numbersArray = new int[10];
+
+            numbersArray[0] = 1;
+            numbersArray[1] = 2;
+            numbersArray[2] = 3;
+            numbersArray[3] = 412;
+            numbersArray[4] = 512;            
+            numbersArray[5] = 15;
+            numbersArray[6] = 53;
+            numbersArray[7] = 125;
+            numbersArray[8] = 533;
+            numbersArray[9] = 125;
+
+            // numbersArray[5] = 6; This will not work, there is no element at 5th position
+
+            // Print out the values
+            Console.WriteLine("The array elements are:");
+            
+            for (int i = 0; i < numbersArray.Length; i++)
+                Console.WriteLine(numbersArray[i]);
+
+            // Non fixed array
+            string[] studentsArray = new string[]
+            {
+                "Milica", "Tamara", "Goran", "Stefan", "Sava"
+            };
+
+            int[] studentsRatingsArray = new int[] { 10, 10, 10, 10, 10 };
+
+            Console.WriteLine("Students: ");
+            
+            // Foreach loop
+            foreach (string student in studentsArray)
+                Console.WriteLine(student);
+
+            Console.WriteLine("Students ratings:");
+            foreach (int rating in studentsRatingsArray)
+                Console.WriteLine(rating);
+
+            #region Exercises
+            // Exercise 1
+            string[] words = new string[] { "Hello", "Hi", "Wow", "Words", "Dinosaur" };
+            double[] decimals = new double[] { 2.4, 5.4444, 0.00009, 18.9, 999.0002 };
+            char[] characters = new char[] { 'b', 'c', '0', '#', ']' };
+            bool[] booleans = new bool[] { true, false, false, true, false };
+            
+            // 2D array example
+            int[][] arraysOfInts = new int[][] {
+                new int[] {2, 4},
+                new int[] {-6, 12},
+                new int[] {0, 99},
+                new int[] {32, 27},
+                new int[] {9999, 50}
+            };
+
+            // Exercise 2 
+            int[] arrayOfIntegers1 = new int[5];
+            int sum1 = 0;
+
+            for (int i = 0; i < arrayOfIntegers1.Length; i++)
+            {
+                Console.WriteLine("Enter index " + i);
+                int number = int.Parse(Console.ReadLine());
+                arrayOfIntegers1[i] = number;
+            }
+            
+            foreach (int num in arrayOfIntegers1) 
+                sum1 += num;
+            
+            Console.WriteLine("The result is: " + sum1);
+
+            // Exercise 2 (Solution 2)
+            int[] arrayOfIntegers2 = new int[5];
+            int sum2 = 0;
+            int j = 0;
+
+            while (j < arrayOfIntegers2.Length)
+            {
+                Console.WriteLine("Enter index " + j);
+
+                sum2 += arrayOfIntegers2[j];
+                j++;
+                
+                if (j == arrayOfIntegers2.Length) 
+                    Console.WriteLine("The result is: " + sum2);
+            }
+
+            // Exercise 3 
+            string[] fullNames = new string[20];
+
+            int counter = 0;
+            while (true)
+            {
+                Console.WriteLine("Please enter a name from the keyboard:");
+                string name = Console.ReadLine();
+
+                fullNames[counter] = name;
+               
+                Console.WriteLine("Do you want to enter another name?");
+
+                if (Console.ReadLine() == "N") 
+                    break;
+            }
+
+            Console.WriteLine("Names stored:");
+
+            foreach (string currentName in fullNames)
+                Console.WriteLine(currentName);
+
+            #endregion
+
+        }
+    }
+}
---
+++ Class03/Loops/Program.cs
@@ -0,0 +1,182 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // FOR - loop example
+
+            Console.WriteLine("FOR Loop: --------------------------------");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"The value of i: {i}");
+            }
+
+            // Infinite for loop
+            for(; ; )
+            {
+                Console.WriteLine("This will go on forver unless you enter X");
+                string userInput = Console.ReadLine();
+
+                if (userInput == "X" || userInput == "x")
+                    break;
+            }
+
+            // forr - reverse for loop
+            for (int i = 10; i > 0; i--)
+                Console.WriteLine($"The value of i: {i}");
+
+            Console.WriteLine("... Happy new year!");
+            Console.WriteLine("We are outside of the loop...");
+
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine("While loop: ------------------------------");
+
+            int counter = 0;
+
+            while (counter < 10)
+            {
+                Console.WriteLine($"The value is: {counter}");
+                counter++;
+            }
+
+            // Infinite while loop
+            string userInputX = string.Empty;
+
+            // Homework - do not execute next iteration if user inputs x or X
+            while (userInputX != "X" && userInputX != "x")
+            {
+                Console.WriteLine("This will go on forver unless you enter X");
+                userInputX = Console.ReadLine();
+            }
+
+            Console.WriteLine("We are outside of the loop...");
+            Console.WriteLine("------------------------------------------");
+
+            Console.WriteLine("Do-While loop: ------------------------------");
+
+            int doWhileCounter = 0;
+
+            do
+            {
+                Console.WriteLine($"The value of counter is {doWhileCounter}");
+                doWhileCounter++;
+
+            } while (doWhileCounter < 10);
+
+            int anotherCounter = 100;
+            
+            do
+            {
+                Console.WriteLine($"Another counter value is {anotherCounter}");
+                anotherCounter++;
+
+            } while (anotherCounter < 10);
+
+            Console.WriteLine("We are outside of the loop...");
+            Console.WriteLine("------------------------------------------");
+
+            // Exercise :D
+
+            /*
+             * Exercise 1:
+               
+               Get an input number from the console​
+               Print all numbers from 1 to that number​
+            
+               Get another input number from the console​
+               Print all numbers from that number to 1​
+            */
+
+            int numA = 0;
+
+            Console.WriteLine("Please input number: ");
+            numA = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 1; i <= numA; i++)
+                Console.Write(i + ", ");
+
+            Console.WriteLine();
+
+            int numB = 0;
+
+            Console.WriteLine("Please input number: ");
+            numB = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = numB; i >= 1; i--)
+                Console.Write(i + ", ");
+
+            /*
+             * Exercise 2:
+             
+               Get an input number from the console​
+               Print all even numbers starting from 2​
+             
+               Get another input number from the console​
+               Print all odd numbers starting from 1​
+             
+             */
+
+            int numC = 0;
+
+            Console.WriteLine("Please input number: ");
+            numC = Convert.ToInt16(Console.ReadLine());
+
+            // #1 solution
+            for (int i = 2; i <= numC; i++)
+            {
+                if (i % 2 == 0)
+                    Console.Write(i + ", ");
+            }
+
+            // #2 solution
+            for (int i = 2; i <= numC; i += 2)
+                Console.Write(i + ", ");
+
+            Console.WriteLine();
+
+            int numD = 0;
+
+            Console.WriteLine("Please input number: ");
+            numD = Convert.ToInt16(Console.ReadLine());
+
+            // #1 solution
+            for (int i = 1; i <= numD; i++)
+            {
+                if (i % 2 != 0)
+                    Console.Write(i + ", ");
+            }
+
+            /*
+             * Exercise 3:
+               Get an input from the console​
+               Print all numbers from 1 to that number​
+               
+               Don’t print numbers that can be divided by 3 or 7​
+               If the number gets to 100 print a message: The limit is reached and stop counting​
+             */
+
+            int numE = 0;
+
+            Console.WriteLine("Please input number: ");
+            numE = Convert.ToInt16(Console.ReadLine());
+
+            // #1 solution
+            for (int i = 1; i <= numE; i++)
+            {
+                if (i % 3 == 0 || i % 7 == 0)
+                    continue;
+
+                Console.Write(i + ", ");
+
+                if (i == 100)
+                {
+                    Console.WriteLine("The limit is reached​");
+                    break;
+                }
+            }
+        }
+    }
+}
